db84aca11e1351a8c406b88b42307266c26e87ea
Sean Devlin <sdevlin@barclaycardus.com>
2017-07-11 10:22:40 -0400

    Made tests.sh run every dir

---
+++ test-data/README.md
@@ -0,0 +1,5 @@
+This is a collection of tests that are run, to ensure that the rules are being interpolated properly.  Each directory is one test suite.  There are the following in each directory
+
+* A config file, conf.jsonw
+* A set of expected keys, in passing-keys.csv
+* A human readable explaination of the test case, in README.md
--- tests.sh
+++ tests.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
 
 TEST_COMMAND="./pattern-getter.rb"
-TEST_DIR=test-data/hello
-CONF_FILE=$TEST_DIR/conf.jsonw
-PASSING_FILE=$TEST_DIR/passing-data.csv
 
 OUTPUT_CODE=0;
-while read -r INPUT EXPECTED_OUTPUT; do
-	ACTUAL_OUTPUT=$($TEST_COMMAND --file $CONF_FILE $INPUT)
-	ACTUAL_CODE=$?
-	RESULT="PASS"
-	if [ "$ACTUAL_CODE" != "0" ]; then
-		OUTPUT_CODE=$((OUTPUT_CODE+1));
-		RESULT="FAIL";
-	elif [ "$ACTUAL_OUTPUT" != "$EXPECTED_OUTPUT" ]; then
-		OUTPUT_CODE=$((OUTPUT_CODE+1));
-		RESULT="FAIL";
-	fi
-	if [ "$RESULT" == "PASS" ]; then
-		echo -e "\x1B[1;32m$RESULT\x1B[0m - Input: '$INPUT' Actual: '$ACTUAL_OUTPUT' Expected: '$EXPECTED_OUTPUT'"
-	else
-		echo -e "\x1B[1;31m$RESULT\x1B[0m - Input: '$INPUT' Actual: '$ACTUAL_OUTPUT' Expected: '$EXPECTED_OUTPUT'"
-	fi
 
-done < <(sed -e 's/#.*//' $PASSING_FILE | grep '.')
+for TEST_DIR in $(find test-data -type d -depth 1); do
+	echo $TEST_DIR
+	CONF_FILE=$TEST_DIR/conf.jsonw
+	PASSING_FILE=$TEST_DIR/passing-data.csv
+	while read -r INPUT EXPECTED_OUTPUT; do
+		ACTUAL_OUTPUT=$($TEST_COMMAND --file $CONF_FILE $INPUT)
+		ACTUAL_CODE=$?
+		RESULT="PASS"
+		if [ "$ACTUAL_CODE" != "0" ]; then
+			OUTPUT_CODE=$((OUTPUT_CODE+1));
+			RESULT="FAIL";
+		elif [ "$ACTUAL_OUTPUT" != "$EXPECTED_OUTPUT" ]; then
+			OUTPUT_CODE=$((OUTPUT_CODE+1));
+			RESULT="FAIL";
+		fi
+		if [ "$RESULT" == "PASS" ]; then
+			echo -e "\x1B[1;32m$RESULT\x1B[0m - Input: '$INPUT' Actual: '$ACTUAL_OUTPUT' Expected: '$EXPECTED_OUTPUT'"
+		else
+			echo -e "\x1B[1;31m$RESULT\x1B[0m - Input: '$INPUT' Actual: '$ACTUAL_OUTPUT' Expected: '$EXPECTED_OUTPUT'"
+		fi
+
+	done < <(sed -e 's/#.*//' $PASSING_FILE | grep '.')
+done
 
 echo "There were $OUTPUT_CODE failing tests"
 if [ "$OUTPUT_CODE" != "0" ]; then
