03033bf5eacabc7917541839e3c790b22689ae12
Sean Devlin <sdevlin@barclaycardus.com>
2017-07-12 07:35:10 -0400

    Renamed data dirs to be sorted for docs

--- tests.sh
+++ tests.sh
@@ -8,6 +8,14 @@ for TEST_DIR in $(find test-data -type d -depth 1); do
 	echo $TEST_DIR
 	CONF_FILE=$TEST_DIR/conf.jsonw
 	PASSING_FILE=$TEST_DIR/passing-data.csv
+	OUTPUT_MD_FILE=$TEST_DIR/output.md
+	INPUT_MD_FILE=$TEST_DIR/README.md
+	rm -f $OUTPUT_MD_FILE
+	echo "# $(basename $TEST_DIR | sed -e 's/[-_]/ /g')" >> $OUTPUT_MD_FILE
+	cat $INPUT_MD_FILE >> $OUTPUT_MD_FILE
+	echo -e "" >> $OUTPUT_MD_FILE
+	sed -e 's/^/    /' $CONF_FILE >> $OUTPUT_MD_FILE
+	echo -e "This will produce the following output\n" >> $OUTPUT_MD_FILE
 	while read -r INPUT EXPECTED_OUTPUT; do
 		ACTUAL_OUTPUT=$($TEST_COMMAND --file $CONF_FILE $INPUT)
 		ACTUAL_CODE=$?
@@ -24,7 +32,8 @@ for TEST_DIR in $(find test-data -type d -depth 1); do
 		else
 			echo -e "\x1B[1;31m$RESULT\x1B[0m - Input: '$INPUT' Actual: '$ACTUAL_OUTPUT' Expected: '$EXPECTED_OUTPUT'"
 		fi
-
+		echo "    $ $TEST_COMMAND $INPUT" >> $OUTPUT_MD_FILE
+		echo -e "    $ACTUAL_OUTPUT\n     " >> $OUTPUT_MD_FILE
 	done < <(sed -e 's/#.*//' $PASSING_FILE | grep '.')
 done
 
