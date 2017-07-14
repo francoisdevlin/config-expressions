0aa23dbff08655b71c8a2b0d1357f20050d21c95
Sean Devlin <sdevlin@barclaycardus.com>
2017-07-14 10:20:59 -0400

    Added the explain command

--- pattern-getter.rb
+++ pattern-getter.rb
@@ -21,6 +21,17 @@ opt_parser = OptionParser.new do |opts|
       end
 end
 
+class String
+	def black; "\e[30m#{self}\e[0m" end
+	def red; "\e[31m#{self}\e[0m" end
+	def green; "\e[32m#{self}\e[0m" end
+	def brown; "\e[33m#{self}\e[0m" end
+	def blue; "\e[34m#{self}\e[0m" end
+	def magenta; "\e[35m#{self}\e[0m" end
+	def cyan; "\e[36m#{self}\e[0m" end
+	def gray; "\e[37m#{self}\e[0m" end
+end
+
 opt_parser.parse!(args)
 conf_path = args[0];
 
@@ -33,6 +44,16 @@ start_state.path = path
 
 result = recursion_2(start_state,config)
 
+
+def explain(state)
+	rule = state.evaluated_path.join(".")
+	if state.state == :complete
+		return "HIT ".green + ": #{rule} VALUE: '#{state.value}'"
+	else
+		return "MISS".red   + ": #{rule}, ignoring any children".gray
+	end
+end
+
 if command == "lookup" 
 	result = result.reject{|a,b| b.state == :missing }
 	if result.nil? or result.size == 0 or result[0][1].state != :complete
@@ -40,5 +61,21 @@ if command == "lookup"
 		exit 1
 	end
 	puts result[0][1].value
+	exit 0
+elsif command == "explain"
+	i = 0
+	print "The following rules were evaluated in this order, the first hit is returned\n"
+	final_result = nil
+	result.each do |pattern,state|
+		i+= 1
+
+		print "Rule %5d: #{explain state}\n" %[i]
+		final_result = state.value if state.state == :complete and final_result.nil?
+	end
+	if final_result.nil?
+		print "Could not find #{conf_path}"
+	else
+		puts final_result
+	end
 	exit 0
 end
